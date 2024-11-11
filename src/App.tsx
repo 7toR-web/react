921e837871dd56e0fda8887a42fac81cdaf32c1a
7toR <kostya5270@gmail.com>
2024-11-11 09:54:54 +0100

    trying

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import './App.css'
-import FirstText from './components/firstComponent'
 
 function App() {
-    return (
-        <>
-            <FirstText />
-        </>
-    )
+    return <></>
 }
 
 export default App
