1a3eea55bcc7899f974bf866f110eb8b8594d22a
7toR <kostya5270@gmail.com>
2024-11-12 21:16:30 +0100

    work with issues in code and hovering over variables and statements

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,15 +1,44 @@
-type Props =  {
-  value: string
+import { title } from 'process'
+
+type Props = {
+    value: string
 }
 
 function FirstTSComponent(props: Props) {
-  const {value} = props
-  
-  return (
-    <>
-      <h1>{value}</h1>
-    </>
-  )
+    const { value } = props
+
+    return (
+        <>
+            <h1>{value}</h1>
+        </>
+    )
 }
 
 export default FirstTSComponent
+
+const joberYana = {
+    job: {
+        // title: 1231                       wrong property type
+        title: `TypeScript development`,
+    },
+}
+
+const logUserJobTitle = (user: { job: { title: string } }) => {
+    console.log(user.job.title)
+}
+
+logUserJobTitle(joberYana)
+
+// let element = document.getElementById(12)
+
+/**
+ * Add two numbers together
+ * @example
+ * myFunction(1,4) 
+ * @returns 5
+ */
+
+const myFunction = (a: number, b: number) => {
+    return a + b
+}
+// hover to explore what does function do ^^^^^^^
