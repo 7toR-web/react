0fa72cae1b6d880ea82f4e8cbe70116c02965cdd
7toR <kostya5270@gmail.com>
2024-11-22 18:19:28 +0100

    renderingList + using Generic

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import React from 'react'
 import './App.css'
 import { Profile } from './components/passing props'
 import { FirstText } from './components/firstComponent'
-import { PackingList } from './conditionlyRendering'
+import { PackingList } from './components/conditionlyRendering'
+import List from './components/renderingLists'
+import { ReceipeLists } from './components/renderingLists'
+import { Separator } from './components/renderingLists'
 
 function App() {
     return (
         <>
             {/* <FirstText /> */}
             {/* <Profile /> */}
-            <PackingList />
+            {/* <PackingList /> */}
+            {/* <List /> */}
+            <ReceipeLists/>
+            <Separator />
         </>
     )
 }
