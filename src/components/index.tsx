eabf91687265fb861840a2a150ddf2775497a076
7toR <kostya5270@gmail.com>
2024-12-03 13:11:27 +0100

    checkbox management

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,1 +1,25 @@
-export{}
+import { useState } from 'react'
+import css from './component.module.css'
+
+const CheckBoxCreater = () => {
+    const [box1State, setBox1State] = useState(false)
+
+    box1State && console.log(`marked`)
+    !box1State && console.log(`unmarked`)
+
+    return (
+        <>
+            <input
+                className={css.checkBoxes}
+                type='checkbox'
+                checked={box1State}
+                onChange={e => {
+                    setBox1State(e.target.checked)
+                }}
+            />
+            <span>CheckBox 1</span>
+        </>
+    )
+}
+
+export { CheckBoxCreater }
