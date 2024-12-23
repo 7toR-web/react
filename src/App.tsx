f3e26fd66110e7e465ccac393f2081ee538bc56c
7toR <kostya5270@gmail.com>
2024-12-23 18:04:28 +0100

    good instances using state and handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './App.css'
+import css from './App.module.css'
 import { Profile } from './describingUI/passing props'
 import { FirstText } from './describingUI/firstComponent'
 import { PackingList } from './describingUI/conditionlyRendering'
@@ -25,6 +25,10 @@ import { ReverseList } from './addingInteractivity/updatingArraysInState/index'
 import { ClickCounter } from './additionalTasks'
 import { ToDoList } from './additionalTasks'
 import { WorkForm } from './additionalTasks/index'
+import { SimpleTimer } from './additionalTasks'
+import { StateCounter } from './additionalTasks'
+import { StateFormUpdate } from './additionalTasks'
+import { Groceries } from './additionalTasks'
 
 function App() {
     return (
@@ -57,6 +61,31 @@ function App() {
             <br />
             <ToDoList />
             <WorkForm />
+            <div className={css.calendar}>
+                {Array.from({ length: 31 }).map((item, index) => {
+                    return <div key={index}>{index + 1}</div>
+                })}
+            </div>
+            <div className={css.try}>
+                <div className={css.header}>Header</div>
+                <div className={css.navigation}>Navigation</div>
+                <div className={css.content}>Content</div>
+                <div className={css.ads}>Ads</div>
+                <div className={css.footer}>Footer</div>
+            </div>
+            <div className={css.firstLoyaut}>
+              <div className={css.firstHeader}>Header</div>
+              <div className={css.firstSideBar}>SideBar</div>
+              <div className={css.firstMain}>Main</div>
+            </div>
+            <div className={css.alignment}>
+              <div>1</div>
+              <div>2</div>
+            </div>
+            <SimpleTimer />
+            <StateCounter />
+            <StateFormUpdate />
+            <Groceries />
         </>
     )
 }
--- src/additionalTasks/index.tsx
+++ src/additionalTasks/index.tsx
@@ -1,6 +1,7 @@
 import css from './additionalTasks.module.css'
 import { useState } from 'react'
 import { useImmer } from 'use-immer'
+import { v4 as uuidv4 } from 'uuid'
 
 const ClickCounter = () => {
     const [counter, setCounter] = useState<number>(0)
@@ -156,3 +157,201 @@ const WorkForm = () => {
 }
 
 export { WorkForm }
+
+const SimpleTimer = () => {
+    const [time, setTime] = useState<number>(0)
+    const [intervalId, setIntervalId] = useState<number | null>(null)
+
+    const handleStartClick = (): void => {
+        if (intervalId === null) {
+            const interval = window.setInterval(() => {
+                setTime(prev => prev + 1)
+            }, 1000)
+            setIntervalId(interval)
+        }
+    }
+
+    const handleStopClick = (): void => {
+        if (intervalId !== null) {
+            clearInterval(intervalId)
+            setIntervalId(null)
+        }
+    }
+
+    const handleResetClick = (): void => {
+        setTime(0)
+    }
+
+    return (
+        <>
+            <p>Time: {time}</p>
+            <button onClick={handleStartClick} style={{ margin: '10px' }}>
+                Start
+            </button>
+            <button onClick={handleStopClick} style={{ margin: '10px' }}>
+                Stop
+            </button>
+            <button onClick={handleResetClick} style={{ margin: '10px' }}>
+                Reset
+            </button>
+        </>
+    )
+}
+
+export { SimpleTimer }
+
+const StateCounter = () => {
+    const [gameState, setGameState] = useState<number>(0)
+    const handleIncrease = () => setGameState(prevState => prevState + 1)
+    const handleDecrease = () => setGameState(prevState => prevState - 1)
+    const handleRandom = () => {
+        const randomNum = Math.floor(Math.random() * 10) + 1
+        setGameState(prevState => prevState + randomNum)
+    }
+
+    return (
+        <>
+            <p>{gameState}</p>
+            <button onClick={handleIncrease} style={{ margin: '10px' }}>
+                Increase 1
+            </button>
+            <button onClick={handleDecrease} style={{ margin: '10px' }}>
+                Decrease 1
+            </button>
+            <button onClick={handleRandom} style={{ margin: '10px' }}>
+                Random
+            </button>
+        </>
+    )
+}
+
+export { StateCounter }
+
+const StateForm = () => {
+    const [person, setPerson] = useState<{ name: string; age: number }>({
+        name: '',
+        age: 0,
+    })
+
+    return (
+        <>
+            <input
+                placeholder='Enter your name'
+                onChange={e => setPerson({ ...person, name: e.target.value })}
+                value={person.name}
+            />
+            <input
+                placeholder='Enter your age'
+                onChange={e =>
+                    setPerson({ ...person, age: Number(e.target.value) })
+                }
+                value={person.age}
+            />
+            <p>
+                {person.name} is {person.age} years old
+            </p>
+        </>
+    )
+}
+
+export { StateForm as StateFormUpdate }
+
+const Groceries = () => {
+    const [itemsList, setItemsList] = useState<
+        { id: string; name: string; quanity: number }[]
+    >([])
+    const [currentItem, setCurrentItem] = useState<{
+        name: string
+        quanity: number
+        id: string
+    }>({ name: '', quanity: 0, id: '' })
+    const [editingItem, setEditingItem] = useState<string | undefined>()
+
+    const handleChanges = (id: string) => {
+        document.getElementById('quanityInput')?.focus()
+        const itemToChange = itemsList.find(item => item.id === id)
+        if (itemToChange) {
+            setCurrentItem({ ...itemToChange })
+            setEditingItem(id)
+        }
+    }
+    const handleDelete = (id: string) => {
+        const newItemsList = itemsList.filter(item => item.id !== id)
+        setItemsList(newItemsList)
+    }
+    const handleAddClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        if (!currentItem.name.trim() || currentItem.quanity <= 0) {
+            alert('Enter valid data!')
+            return
+        }
+        if (editingItem) {
+            setItemsList(
+                itemsList.map(item =>
+                    item.id === editingItem ? { ...currentItem } : item
+                )
+            )
+            setEditingItem(undefined)
+        } else {
+            setItemsList([...itemsList, { ...currentItem, id: uuidv4() }])
+        }
+        setCurrentItem({ name: '', quanity: 0, id: '' })
+    }
+    const handleCurrentItemName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCurrentItem({
+            ...currentItem,
+            name: e.target.value,
+        })
+    }
+    const handleCurrentItemQuanity = (
+        e: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setCurrentItem({
+            ...currentItem,
+            quanity: Number(e.target.value),
+        })
+    }
+
+    return (
+        <>
+            <input
+                placeholder='Enter item name'
+                onChange={handleCurrentItemName}
+                value={currentItem.name}
+                id='nameInput'
+            />
+            <input
+                placeholder='Enter item quantity'
+                onChange={handleCurrentItemQuanity}
+                value={currentItem.quanity}
+                id='quanityInput'
+            />
+            <button onClick={handleAddClick} id='addButton'>
+                {editingItem ? 'Save' : 'Add'}
+            </button>
+            <ul>
+                {itemsList.map((item, index) => {
+                    return (
+                        <li key={item.id}>
+                            {item.name}: {item.quanity}
+                            <button
+                                style={{ marginLeft: '10px' }}
+                                onClick={() => handleChanges(item.id)}
+                                id='changeButton'
+                            >
+                                Change
+                            </button>
+                            <button
+                                style={{ marginLeft: '10px' }}
+                                onClick={() => handleDelete(item.id)}
+                                id='deleteButton'
+                            >
+                                Delete
+                            </button>
+                        </li>
+                    )
+                })}
+            </ul>
+        </>
+    )
+}
+export { Groceries }
